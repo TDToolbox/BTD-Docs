d5802a8f23167848d688fc7285bf4d3e4996b83b
Gurrenm3 <ThomasPMar@hotmail.com>
2020-09-25 17:27:38 -0700

    Added Better FastForward mod to NKH Example mods

---
+++ NK-Unity Engine/Btd6/NKHook6/Example Mods/Better FastForward/Main.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Scripts.Utils;
+using MelonLoader;
+using NKHook6;
+using NKHook6.Api.Events;
+using NKHook6.Api.Events._TimeManager;
+
+namespace BetterFastForward
+{
+    public class Main : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            EventRegistry.subscriber.register(this.GetType());
+
+            NKHook6.Logger.Log("Better FastForward loaded!");
+        }
+
+
+        [EventAttribute("TimeManagerSetFastForwardEvent.Post")]
+        public static void ChangeFastForward(TimeManagerSetFastForwardEvent.Postfix e)
+        {
+            if (TimeManager.gamePaused || TimeManager.coopPaused)
+                return;
+
+            Settings settings =  NKHook6.Api.Utilities.JsonUtils2.Load<Settings>();
+            
+            TimeManager.compromisedFastFowardRate = (int)settings.newSpeed;
+            TimeManager.maxSimulationStepsPerUpdate = settings.newSpeed;
+        }
+    }
+}
---
+++ NK-Unity Engine/Btd6/NKHook6/Example Mods/Better FastForward/Settings.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using NKHook6.Api;
+using NKHook6.Api.Utilities;
+using System;
+using System.IO;
+
+namespace BetterFastForward
+{
+    public class Settings : ModSettings
+    {
+        public float newSpeed { get; set; } = 10;
+    }
+}
