be9d024d2d097f5a4b19cd471e5f5a93c1d0d877
Gurrenm3 <ThomasPMar@hotmail.com>
2020-09-25 17:42:45 -0700

    Updated Better FastForward and added comments

--- NK-Unity Engine/Btd6/NKHook6/Example Mods/Better FastForward/Main.cs
+++ NK-Unity Engine/Btd6/NKHook6/Example Mods/Better FastForward/Main.cs
@@ -1,32 +1,34 @@
 ﻿using Assets.Scripts.Utils;
 using MelonLoader;
-using NKHook6;
 using NKHook6.Api.Events;
 using NKHook6.Api.Events._TimeManager;
 
 namespace BetterFastForward
 {
-    public class Main : MelonMod
+    public class Main : MelonMod    //Make sure you have   : MelonMod   here otherwise your mod won't work
     {
-        public override void OnApplicationStart()
+        public override void OnApplicationStart() //Default MelonLoader patch that gets executed when BTD6 starts
         {
             base.OnApplicationStart();
-            EventRegistry.subscriber.register(this.GetType());
+            EventRegistry.subscriber.register(this.GetType());  //Subscribe to NKHook6's custom event system
 
-            NKHook6.Logger.Log("Better FastForward loaded!");
+            NKHook6.Logger.Log("Better FastForward loaded!");  //Post a message to console to tell the user your mod has loaded
         }
 
 
-        [EventAttribute("TimeManagerSetFastForwardEvent.Post")]
-        public static void ChangeFastForward(TimeManagerSetFastForwardEvent.Postfix e)
+        //This is an example of using one of NKHook6's custom events
+        //This example mod gets reads the settings file every time you turn on fast forward.
+        //This means you can change the fast forward speed in your settings and have it update in game!
+        [EventAttribute("TimeManager.SetFastForwardEvent.Post")]
+        public static void ChangeFastForward(SetFastForwardEvent.Post e)
         {
-            if (TimeManager.gamePaused || TimeManager.coopPaused)
+            if (TimeManager.gamePaused || TimeManager.coopPaused)   //exit this method if game is paused
                 return;
 
-            Settings settings =  NKHook6.Api.Utilities.JsonUtils2.Load<Settings>();
+            Settings settings =  NKHook6.Api.Utilities.JsonUtils.Load<Settings>();  //load settings from file
             
-            TimeManager.compromisedFastFowardRate = (int)settings.newSpeed;
-            TimeManager.maxSimulationStepsPerUpdate = settings.newSpeed;
+            TimeManager.compromisedFastFowardRate = (int)settings.newSpeed; //Set current speed. Convert newSpeed from a float to int
+            TimeManager.maxSimulationStepsPerUpdate = settings.newSpeed;    //Set max speed to the same as new current speed
         }
     }
 }
--- NK-Unity Engine/Btd6/NKHook6/Example Mods/Better FastForward/Settings.cs
+++ NK-Unity Engine/Btd6/NKHook6/Example Mods/Better FastForward/Settings.cs
@@ -1,13 +1,13 @@
-﻿using Newtonsoft.Json;
-using NKHook6.Api;
-using NKHook6.Api.Utilities;
-using System;
-using System.IO;
+﻿using NKHook6.Api;
 
 namespace BetterFastForward
 {
+    //This file is an example of how to set up ModSettings for your mod
+    //You can use this to very easily add settings to your mods that are stored locally
+    //in your mod's folder. Can be used to change mod settings in game and out of game
+    //This will be saved to file so you can change it out of game.
     public class Settings : ModSettings
     {
-        public float newSpeed { get; set; } = 10;
+        public float newSpeed { get; set; } = 10;   // A variable I set for speed. Can be changed in "BTD6/Mods/Better FastForward/settings.json"
     }
 }
